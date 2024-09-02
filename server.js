43347300683052d271af4518de381a405517b8d9
Cxndr <livewellandmakethings@gmail.com>
2024-09-02 14:00:25 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+/* ----- import express library and execute it, creating a new express application call "app" -----*/
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, function() {
+    console.log("Server is listening on port 3000...");
+});
+
+app.get ("/messages", function(request, response) {
+    response.json({ message: "Hello, World!" });
+});
+
+app.post("/messages", function (request, response) {
+    response.json({message: `Your message was ${request.body.msg}. So cool`});
+});
+
+// send post request for {"msg": "####"} to get response
